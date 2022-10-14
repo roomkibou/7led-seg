d5fd86d1dceec38e8b761dddbb7a15b5bb6bddf9
roomkibou <113073945+roomkibou@users.noreply.github.com>
2022-10-14 17:32:11 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,24 @@
+let _7seg = 0
+pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13)
+pins.spiFormat(8, 3)
+pins.spiFrequency(1000000)
+let 配列 = [
+63,
+6,
+91,
+79,
+102,
+109,
+125,
+7,
+127,
+111
+]
+basic.forever(function () {
+    for (let カウンター = 0; カウンター <= 9; カウンター++) {
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        _7seg = pins.spiWrite(配列[カウンター])
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(1000)
+    }
+})
